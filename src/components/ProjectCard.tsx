904424f107ef3e227c23d76413c6ac3383351185
NATAN <natan.almeida03@gmail.com>
2024-08-27 21:56:40 -0300

    link fix

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -18,7 +18,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
     <div className="max-w-80 bg-bk2 p-5 rounded-2xl flex flex-col gap-7 text-white ">
       <h3 className="font-bold text-xl">{title}</h3>
       <img src={image} alt={title} />
-      <a href={link} className="text-lg my-8 hover:text-yll before:content-['🔗']"> Visitar Repositório</a>
+      <a href={link} target="blank" className="text-lg my-8 hover:text-yll before:content-['🔗']"> Visitar Repositório</a>
       <ul className="flex gap-2 flex-wrap">
         {linguagens.map((linguagem, index) => (
           <li key={index} className="inline bg-bk1 rounded-md px-1">{linguagem}</li>
