94bc73519d204a520b807ae450e56aa51caa1f6c
natanalmeida03 <natan.almeida03@gmail.com>
2025-05-23 11:25:28 -0300

    fix: fix build

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,7 @@ function About() {
       <div className="mx-auto max-w-5xl">
       <Link href="#about"><h2 className="font-bold text-4xl text-pk dark:text-white mb-10 pl-4 md:pr-0">{'About'}</h2></Link>
         <p className="text-xl max-w-3xl text-black dark:text-white pl-4 md:pr-0">
-          I'm Natan, software engineer student from UnB.
-          I'm looking for my first opportunity in the market as a developer.
-          Always seeking new knowledge and improving my skills.
+          { "I'm Natan, software engineer student from UnB. I'm looking for my first opportunity in the market as a developer. Always seeking new knowledge and improving my skills."}
         </p>
       </div>
     </section>
