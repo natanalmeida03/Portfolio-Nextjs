0bafa84b84a0d8866720182eaee0906f0e0f446f
natanalmeida03 <natan.almeida03@gmail.com>
2025-07-24 19:26:06 -0300

    feat(ts): add link to blog page on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
+import { ArrowRight } from "lucide-react";
+
 function Footer() {
   return (
-    <footer className="bg-bk4 dark:bg-bk2 h-44 text-center pt-6">
+    <footer className="bg-bk4 dark:bg-bk2 text-center pt-6 pb-6">
       <div className="flex gap-5 w-fit mx-auto h-10 mb-6">
         <a href="https://github.com/natanalmeida03" target="_blank" className="hover:-translate-y-2 dark:invert transition-all duration-300">
           <img src="/github_c.png" alt="github" className="h-full" />
@@ -12,8 +14,13 @@ function Footer() {
           <img src="/twitter.png" alt="" className="h-full" />
         </a>
       </div>
+      <div>
+        <p className="block"><a href="/me" className="text-black dark:text-white inline-flex items-center mt-6 px-4 py-2 bg-blue-600 rounded-xl hover:text-bk2 hover:dark:text-bk3"> See my personal blog <ArrowRight className="w-4 h-4 mr-2" /></a></p>
+      </div>
+      <br />
       <p className="text-black dark:text-white">© 2024 - presente</p>
       <p className="text-black dark:text-white">Feito com ❤️ por natan</p>
+
     </footer>
   );
 }
