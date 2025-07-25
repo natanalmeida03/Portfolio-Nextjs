9916dd07f5e7e4f95f7740cef0faaa1aebce6e62
natanalmeida03 <natan.almeida03@gmail.com>
2025-07-24 22:12:48 -0300

    fix(ts): fix footer on mobile

--- src/components/me/Footer.tsx
+++ src/components/me/Footer.tsx
@@ -3,7 +3,7 @@ import { ArrowRight } from "lucide-react";
 function Footer() {
   return (
     <footer className="bg-bk4 dark:bg-bk2 pt-6 pb-6">
-        <div className="mx-auto max-w-3xl px-6 flex items-center justify-between">
+        <div className="mx-auto max-w-3xl px-6 flex flex-col gap-5 md:flex-row md:gap-0 items-center justify-between">
             <div className="flex gap-5 h-10">
                 <a href="https://github.com/natanalmeida03" target="_blank" className="hover:-translate-y-2 dark:invert transition-all duration-300">
                 <img src="/github_c.png" alt="github" className="h-full" />
@@ -15,7 +15,7 @@ function Footer() {
                 <img src="/twitter.png" alt="" className="h-full" />
                 </a>
             </div>
-            <div className="text-right">
+            <div className="text-center md:text-right">
                 <p className="text-black dark:text-white">© 2024 - presente</p>
                 <p className="text-black dark:text-white">Feito com ❤️ por natan</p>
             </div>
